55b88ee63e0e33e459d132053f4350adb3234c89
Henry Schimke <henry@azule.info>
2023-12-23 10:14:27 -0600

    test: add tests associated with group parsing issues

--- src/lib.rs
+++ src/lib.rs
@@ -267,6 +267,152 @@ mod tests {
     }
 
     #[test]
+    fn it_reads_group_and_single_color_with_explicit_group_end_size() {
+        let group = Group::new(
+            "group name".to_owned(),
+            vec![
+                ColorBlock::new(
+                    "light grey".to_owned(),
+                    ColorValue::Gray(0.5),
+                    ColorType::Normal,
+                ),
+                ColorBlock::new(
+                    "dark red".to_owned(),
+                    ColorValue::Rgb(0.5, 0.3, 0.1),
+                    ColorType::Normal,
+                ),
+            ],
+        );
+        let block = ColorBlock::new("name".to_owned(), ColorValue::Gray(0.5), ColorType::Normal);
+        let input_ase_bytes = create_ase(vec![group.clone()], vec![block.clone()]);
+        let mut modified_ase_bytes = vec![0; 0];
+
+        // The following code modifies the generated ASE bytes so that
+        // the GroupEnd block is followed by a four byte zero length
+        // specifier. The slice lengths presented here are specific
+        // to the test and not general numbers.
+        // The layout, in this case, is:
+        //      bytes 0   - 127       GroupStart,ColorBlocks,GroupEnd
+        //      bytes 128 - end       Global Colors
+        // The modified data has the layout:
+        //      bytes 0   - 127       GroupStart,ColorBlocks,GroupEnd
+        //      bytes 128 - 131       u32(0)
+        //      bytes 132 - end       Global Colors
+        modified_ase_bytes.extend_from_slice(&input_ase_bytes[..128]);
+        modified_ase_bytes.extend_from_slice(&[0; 4]);
+        modified_ase_bytes.extend_from_slice(&input_ase_bytes[128..]);
+        let res = read_ase(&*input_ase_bytes);
+        assert!(res.is_ok());
+        let res = res.unwrap();
+        assert_eq!(res, (vec![group], vec![block]));
+    }
+
+    #[test]
+    fn it_reads_group_and_single_color_with_group_block_name_only_size() {
+        let group = Group::new(
+            "group name".to_owned(),
+            vec![
+                ColorBlock::new(
+                    "light grey".to_owned(),
+                    ColorValue::Gray(0.5),
+                    ColorType::Normal,
+                ),
+                ColorBlock::new(
+                    "dark red".to_owned(),
+                    ColorValue::Rgb(0.5, 0.3, 0.1),
+                    ColorType::Normal,
+                ),
+            ],
+        );
+        let block = ColorBlock::new("name".to_owned(), ColorValue::Gray(0.5), ColorType::Normal);
+        let input_ase_bytes = create_ase(vec![group.clone()], vec![block.clone()]);
+        let mut modified_ase_bytes = vec![0; 0];
+
+        // The following code modifies the generated ASE bytes so that
+        // the GroupStart block is sized to contain only the name
+        // of the group. This requires that the total number of blocks
+        // be updated to include the colors present in the group.
+        // The slice lengths presented here are specific
+        // to the test and not general numbers.
+        // The layout, in this case, is:
+        //      bytes 0   - 7         Header
+        //      bytes 8   - 11        2 expected blocks
+        //      bytes 12  - 13        GroupStart
+        //      bytes 14  - 17        Group block size 108
+        //      bytes 18  - end       Groups and Colors
+        // The modified data has the layout:
+        //      bytes 0   - 7         Header
+        //      bytes 8   - 11        5 expected blocks
+        //      bytes 12  - 13        GroupStart
+        //      bytes 14  - 17        Group block size 24
+        //      bytes 18  - end       Groups and Colors
+        modified_ase_bytes.extend_from_slice(&input_ase_bytes[..8]);
+        modified_ase_bytes.extend_from_slice(&(5_u32.to_be_bytes()));
+        modified_ase_bytes.extend_from_slice(&input_ase_bytes[12..14]);
+        modified_ase_bytes.extend_from_slice(&(24_u32.to_be_bytes()));
+        modified_ase_bytes.extend_from_slice(&input_ase_bytes[18..]);
+        let res = read_ase(&*modified_ase_bytes);
+        assert!(res.is_ok());
+        let res = res.unwrap();
+        assert_eq!(res, (vec![group], vec![block]));
+    }
+
+    #[test]
+    fn it_reads_group_and_single_color_with_group_block_name_only_size_and_explicit_group_end_size()
+    {
+        let group = Group::new(
+            "group name".to_owned(),
+            vec![
+                ColorBlock::new(
+                    "light grey".to_owned(),
+                    ColorValue::Gray(0.5),
+                    ColorType::Normal,
+                ),
+                ColorBlock::new(
+                    "dark red".to_owned(),
+                    ColorValue::Rgb(0.5, 0.3, 0.1),
+                    ColorType::Normal,
+                ),
+            ],
+        );
+        let block = ColorBlock::new("name".to_owned(), ColorValue::Gray(0.5), ColorType::Normal);
+        let input_ase_bytes = create_ase(vec![group.clone()], vec![block.clone()]);
+        let mut modified_ase_bytes = vec![0; 0];
+
+        // The following code modifies the generated ASE bytes so that
+        // the GroupStart block is sized to contain only the name
+        // of the group. This requires that the total number of blocks
+        // be updated to include the colors present in the group.
+        // The slice lengths presented here are specific
+        // to the test and not general numbers.
+        // The layout, in this case, is:
+        //      bytes 0   - 7         Header
+        //      bytes 8   - 11        2 expected blocks
+        //      bytes 12  - 13        GroupStart
+        //      bytes 14  - 17        Group block size 108
+        //      bytes 18  - end       Groups and Colors
+        // The modified data has the layout:
+        //      bytes 0   - 7         Header
+        //      bytes 8   - 11        5 expected blocks
+        //      bytes 12  - 13        GroupStart
+        //      bytes 14  - 17        Group block size 24
+        //      bytes 18  - 127       Groups and Sub Colors
+        //      bytes 128 - 131       u32(0)
+        //      bytes 132 - end       Global colors
+        modified_ase_bytes.extend_from_slice(&input_ase_bytes[..8]);
+        modified_ase_bytes.extend_from_slice(&(5_u32.to_be_bytes()));
+        modified_ase_bytes.extend_from_slice(&input_ase_bytes[12..14]);
+        modified_ase_bytes.extend_from_slice(&(24_u32.to_be_bytes()));
+        modified_ase_bytes.extend_from_slice(&input_ase_bytes[18..128]);
+        modified_ase_bytes.extend_from_slice(&[0; 4]);
+        modified_ase_bytes.extend_from_slice(&input_ase_bytes[128..]);
+        let res = read_ase(&*modified_ase_bytes);
+        assert!(res.is_ok());
+        let res = res.unwrap();
+        assert_eq!(res, (vec![group], vec![block]));
+    }
+
+    #[test]
     fn it_returns_incorrect_block_type_error() {
         let input_bad_block_type = vec![
             65, 83, 69, 70, 0, 1, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 22, 0, 5, 0, 110, 0, 97, 0, 109,
